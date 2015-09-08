283af717338d4b23005b8e8e9ef55c1f7feb590c
michael.parker <michael.parker@grantadesign.com>
2015-09-08 17:43:40 +0100

    Rename main form

--- Program.cs
+++ Program.cs
@@ -16,7 +16,7 @@ namespace JiraGitHubPRCreator
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new Form1());
+            Application.Run(new JiraGitHubPRCreator());
         }
     }
 }
