d0c34f8592e0b85af7bc3cb39a74ddbd3c84011e
Chris Patrick <chris.patrick@cantab.net>
2015-09-30 23:52:17 +0100

    Look! A Web frontend!

---
+++ JiraGitHubPRCreator.Web/Controllers/HomeController.cs
@@ -0,0 +1,43 @@
+﻿using JiraGitHubPRCreator.Core.GitHub;
+using JiraGitHubPRCreator.Web.Models;
+using System.Collections.Generic;
+using System.Web.Mvc;
+
+namespace JiraGitHubPRCreator.Web.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult Submit(PRRequestModel model)
+        {
+            var linkedPrCreator = new LinkedPrCreator(model.PersonalAccessToken, model.BranchName, model.JiraIssueId, model.PullRequestTitle, model.Description, "grantadesign", "mi", new WebUserNotifier());
+
+            var branchDefinitions = new List<BranchDefinition>();
+
+            if (model.MIvNext)
+            {
+                branchDefinitions.Add(new BranchDefinition("next", "next"));
+            }
+            if (model.MIv81)
+            {
+                branchDefinitions.Add(new BranchDefinition("releases/8.1/next", "8.1"));
+            }
+            if (model.MIv80)
+            {
+                branchDefinitions.Add(new BranchDefinition("releases/8.0/next", "8.0"));
+            }
+            if (model.MIv7)
+            {
+                branchDefinitions.Add(new BranchDefinition("releases/7.0/next", "7.0"));
+            }
+
+            linkedPrCreator.MakeLinkedPullRequests(branchDefinitions, model.AddLinksToJira, model.SetJiraIssuePendingMerge);
+
+            return View("Index");
+        }
+    }
+}
---
+++ JiraGitHubPRCreator.Web/Models/PRRequestModel.cs
@@ -0,0 +1,17 @@
+﻿namespace JiraGitHubPRCreator.Web.Models
+{
+    public class PRRequestModel
+    {
+        public string BranchName { get; set; }
+        public string JiraIssueId { get; set; }
+        public string PullRequestTitle { get; set; }
+        public string Description { get; set; }
+        public bool MIv7 { get; set; }
+        public bool MIv80 { get; set; }
+        public bool MIv81 { get; set; }
+        public bool MIvNext { get; set; }
+        public bool AddLinksToJira { get; set; }
+        public bool SetJiraIssuePendingMerge { get; set; }
+        public string PersonalAccessToken { get; set; }
+    }
+}
